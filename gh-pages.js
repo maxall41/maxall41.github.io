1b5c37378c1520d92d645533be8db45e889279ab
Max Campbell <maxall4@protonmail.com>
2022-03-19 18:25:00 -1000

    Changes

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ import { publish } from 'gh-pages';
 publish(
 	'build', // path to public directory
 	{
-		branch: 'gh-pages',
+		branch: 'main',
 		repo: 'https://github.com/maxall41/maxall41.github.io.git', // Update to point to your repository
 		user: {
 			name: 'Max Campbell', // update to use your name
