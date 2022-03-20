35a164dd3c12f27840657d22919b68df9f3b4946
Max Campbell <maxall4@protonmail.com>
2022-03-19 19:19:20 -1000

    Changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,9 +9,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
+			out: 'build'
 		})
 	}
 };
